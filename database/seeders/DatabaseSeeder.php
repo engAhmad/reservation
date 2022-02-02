4bdf2478abbe1fceee60f116296339c5bceb72b7
ahmad <engAhmad@users.noreply.github.com>
2022-02-02 17:50:14 +0200

    fix removed base controller
    base controller , update seeders , add root route

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CarSeeder::class);
+        $this->call(MiniHouseSeeder::class);
+        $this->call(PackageSeeder::class);
     }
 }
